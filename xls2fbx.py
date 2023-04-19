38bcc2b680b18acd5efc78e8514502044dac5299
JeziL <wangjinlithu@gmail.com>
2023-04-19 12:08:39 +0800

    输出文件默认为输入文件同名

--- xls2fbx.py
+++ xls2fbx.py
@@ -1,3 +1,4 @@
+import os
 import fbx_writer
 from xls_dbreader import XLSDatabase
 from argparse import ArgumentParser
@@ -11,11 +12,14 @@ if __name__ == "__main__":
         epilog="Deployment icons created by Uniconlabs - Flaticon",
     )
     parser.add_argument("input_file", help="Excel 工作簿文件")
-    parser.add_argument("-t", "--template", help="FlexRay 数据库模板文件")
+    parser.add_argument("-t", "--template", help="FlexRay 数据库模板文件", required=True)
     parser.add_argument("-o", "--output", help="生成数据库文件的输出路径")
     parser.add_argument(
         "-v", "--version", action="version", version=f"%(prog)s {__version__}"
     )
     args = parser.parse_args()
+    if args.output is None:
+        basename, old_suffix = os.path.splitext(args.input_file)
+        args.output = basename + ".xml"
     db = XLSDatabase(args.input_file)
     fbx_writer.write(args.template, db, args.output)
